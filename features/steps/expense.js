7f3aaa7853f647c6c3d57b7b286821a321cdad42
nadirbasalamah <nadir008basalamah@gmail.com>
2024-01-13 08:08:30 +0700

    feat(test): convert to Page Object Model

--- features/steps/expense.js
+++ features/steps/expense.js
@@ -1,83 +1,48 @@
 import { Given, When, Then, Before, After } from "@cucumber/cucumber";
-import initDriver from "../../configs/driver.js";
 import assert from "assert";
 
+import initDriver from "../../configs/driver.js";
+import ExpenseFormPage from "../../pages/form.js";
+
 let driver;
+let expenseFormPage;
 
 Before({ timeout: 6000 * 10000 }, async () => {
   try {
     driver = await initDriver();
+    expenseFormPage = new ExpenseFormPage(driver);
   } catch (error) {
     console.error(error);
   }
 });
 
 Given("I am on the home page", async () => {
-  const titleComponent = await driver.$(
-    '//android.view.View[@content-desc="Personal Expenses"]'
-  );
-  const title = await titleComponent.getAttribute("content-desc");
+  const title = await expenseFormPage.getTitle();
   assert.equal(title, "Personal Expenses");
 });
 
 When("I tap plus button", async () => {
-  const addButton = await driver.$(
-    "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.View/android.view.View/android.view.View/android.view.View/android.widget.Button"
-  );
-  await addButton.click();
+  await expenseFormPage.tapPlusButton();
 });
 
 Then("I insert title", async () => {
-  const titleInput = await driver.$(
-    '//android.view.View[@content-desc="Picked Date: 1/7/2024"]/android.widget.EditText[1]'
-  );
-
-  await titleInput.click();
-  await titleInput.setValue("my expense");
-  await driver.back();
+  await expenseFormPage.insertTitle("my expense");
 });
 
 Then("I insert amount", async () => {
-  const amountInput = await driver.$(
-    '//android.view.View[@content-desc="Picked Date: 1/7/2024"]/android.widget.EditText[2]'
-  );
-
-  await amountInput.click();
-  await amountInput.setValue(12500);
-  await driver.back();
+  await expenseFormPage.insertAmount(12500);
 });
 
 Then("I pick a date", async () => {
-  const datePicker = await driver.$(
-    '//android.widget.Button[@content-desc="Choose Date"]'
-  );
-
-  await datePicker.click();
-
-  const okButton = await driver.$(
-    '//android.widget.Button[@content-desc="OK"]'
-  );
-
-  await okButton.click();
-  await driver.back();
+  await expenseFormPage.pickDate();
 });
 
 Then("I tap add transaction button", async () => {
-  const addTxButton = await driver.$(
-    '//android.widget.Button[@content-desc="Add Transaction"]'
-  );
-
-  await addTxButton.click();
+  await expenseFormPage.tapAddTxButton();
 });
 
 Then("my expense is added", async () => {
-  const expense = await driver.$(
-    `//android.view.View[@content-desc="$12500.0
-my expense
-Jan 7, 2024"]`
-  );
-
-  const expenseContent = await expense.getAttribute("content-desc");
+  const expenseContent = await expenseFormPage.getInsertedData();
   assert.notEqual(expenseContent, "");
 });
 
---
+++ pages/form.js
@@ -0,0 +1,109 @@
+import BasePage from "./base.js";
+
+export default class ExpenseFormPage extends BasePage {
+  constructor(driver) {
+    super(driver);
+  }
+
+  getCurrentDate() {
+    const currentDate = new Date();
+
+    const day = currentDate.getDate();
+    const month = currentDate.getMonth() + 1;
+    const year = currentDate.getFullYear();
+
+    const formattedDate = `${month}/${day}/${year}`;
+
+    return formattedDate;
+  }
+
+  getExpenseDate() {
+    const currentDate = new Date();
+
+    const months = [
+      "Jan",
+      "Feb",
+      "Mar",
+      "Apr",
+      "May",
+      "Jun",
+      "Jul",
+      "Aug",
+      "Sep",
+      "Oct",
+      "Nov",
+      "Dec",
+    ];
+
+    const day = currentDate.getDate();
+    const monthIndex = currentDate.getMonth();
+    const year = currentDate.getFullYear();
+
+    const monthName = months[monthIndex];
+
+    const formattedDate = `${monthName} ${day}, ${year}`;
+
+    return formattedDate;
+  }
+
+  async getTitle() {
+    const titleComponent = await this.find(
+      '//android.view.View[@content-desc="Personal Expenses"]'
+    );
+    const title = await titleComponent.getAttribute("content-desc");
+    return title;
+  }
+
+  async tapPlusButton() {
+    await this.click(
+      "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.View/android.view.View/android.view.View/android.view.View/android.widget.Button"
+    );
+  }
+
+  async insertTitle(title) {
+    await this.click(
+      `//android.view.View[@content-desc="Picked Date: ${this.getCurrentDate()}"]/android.widget.EditText[1]`
+    );
+
+    await this.type(
+      `//android.view.View[@content-desc="Picked Date: ${this.getCurrentDate()}"]/android.widget.EditText[1]`,
+      title
+    );
+
+    await this.driver.back();
+  }
+
+  async insertAmount(amount) {
+    await this.click(
+      `//android.view.View[@content-desc="Picked Date: ${this.getCurrentDate()}"]/android.widget.EditText[2]`
+    );
+
+    await this.type(
+      `//android.view.View[@content-desc="Picked Date: ${this.getCurrentDate()}"]/android.widget.EditText[2]`,
+      amount
+    );
+
+    await this.driver.back();
+  }
+
+  async pickDate() {
+    await this.click('//android.widget.Button[@content-desc="Choose Date"]');
+    await this.click('//android.widget.Button[@content-desc="OK"]');
+    await this.driver.back();
+  }
+
+  async tapAddTxButton() {
+    await this.click(
+      '//android.widget.Button[@content-desc="Add Transaction"]'
+    );
+  }
+
+  async getInsertedData() {
+    const element = await this.find(`//android.view.View[@content-desc="$12500.0
+my expense
+${this.getExpenseDate()}"]`);
+
+    const data = await element.getAttribute("content-desc");
+    return data;
+  }
+}
